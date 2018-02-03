0c1cde2022fae2880adb2df1bc86da0fb3df412e
Lucas Hanke <hankelucas@gmail.com>
2018-02-03 17:15:59 -0200

    add missing file

---
+++ app/components/common/DatePicker.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ExternalDatePicker from 'react-native-datepicker';
+import { StyleSheet } from 'react-native';
+
+import appStyles, { RED, DARK_PURPLE } from '../../styles';
+
+export const style = StyleSheet.create({
+  btnTextCancel: {
+    color: RED,
+    fontSize: 18,
+  },
+  btnTextConfirm: {
+    color: DARK_PURPLE,
+    fontSize: 18,
+  },
+  date: {
+    width: '100%',
+  },
+});
+
+const DatePicker = props => (
+  <ExternalDatePicker
+    {...props}
+    mode="date"
+    format="DD/MMM/YYYY"
+    confirmBtnText="Ok"
+    cancelBtnText="Cancelar"
+    showIcon={false}
+    style={style.date}
+    customStyles={{
+      dateInput: appStyles.input,
+      btnTextCancel: style.btnTextCancel,
+      btnTextConfirm: style.btnTextConfirm,
+    }}
+  />
+);
+
+export default DatePicker;
