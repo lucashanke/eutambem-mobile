e5c1036b9d7455d7d84499118b6b639a8b2ff077
Lucas Hanke <hankelucas@gmail.com>
2018-03-24 13:22:52 -0300

    [N/A]: extract options to constants file

--- app/components/CreateReportForm.js
+++ app/components/CreateReportForm.js
@@ -7,8 +7,13 @@ import DatePicker from '../components/common/DatePicker';
 import PlacePicker from '../components/common/PlacePicker';
 
 import SectionHeader from './common/SectionHeader';
-
-const MAX_TEXT_INPUT_LENGTH = 350;
+import { HARASSMENT_TYPE_OPTIONS,
+  YES_NO_OPTIONAL_OPTIONS,
+  GENDER_OPTIONS,
+  SKIN_COLOR_OPTIONS,
+  AGE_OPTIONS,
+  WAGE_OPTIONS,
+  MAX_TEXT_INPUT_LENGTH } from '../constants';
 
 export default class CreateReportForm extends Component {
   constructor(props) {
@@ -33,25 +38,7 @@ export default class CreateReportForm extends Component {
         />
         <Picker
           placeholder="Tipo de assédio"
-          items={[{
-            value: 'physical',
-            label: 'Assédio Físico',
-          }, {
-            value: 'verbal',
-            label: 'Assédio Verbal',
-          }, {
-            value: 'homophobia',
-            label: 'Homofobia',
-          }, {
-            value: 'racism',
-            label: 'Racismo',
-          }, {
-            value: 'transfobia',
-            label: 'Transfobia',
-          }, {
-            value: 'other',
-            label: 'Outro tipo de Assédio',
-          }]}
+          items={HARASSMENT_TYPE_OPTIONS}
         />
         <TextInput
           style={[appStyles.input, appStyles.multilineInput]}
@@ -69,16 +56,7 @@ export default class CreateReportForm extends Component {
         />
         <Picker
           placeholder="Você recomendaria essa empresa depois do ocorrido?"
-          items={[{
-            value: 'yes',
-            label: 'Sim',
-          }, {
-            value: 'no',
-            label: 'Não',
-          }, {
-            value: 'notInformed',
-            label: 'Prefiro não responder',
-          }]}
+          items={YES_NO_OPTIONAL_OPTIONS}
         />
         <TextInput
           style={[appStyles.input, appStyles.multilineInput]}
@@ -92,82 +70,19 @@ export default class CreateReportForm extends Component {
         <SectionHeader title="Sobre você" />
         <Picker
           placeholder="Gênero"
-          items={[{
-            value: 'woman',
-            label: 'Mulher',
-          }, {
-            value: 'man',
-            label: 'Homem',
-          }, {
-            value: 'nonbinary',
-            label: 'Não binário',
-          }]}
+          items={GENDER_OPTIONS}
         />
         <Picker
           placeholder="Cor"
-          items={[{
-            value: 'black',
-            label: 'Negra',
-          }, {
-            value: 'white',
-            label: 'Branca',
-          }, {
-            value: 'brown',
-            label: 'Parda',
-          }, {
-            value: 'asian',
-            label: 'Amarela',
-          }, {
-            value: 'indian',
-            label: 'Indígena',
-          }, {
-            value: 'notInformed',
-            label: 'Prefiro não dizer',
-          }]}
+          items={SKIN_COLOR_OPTIONS}
         />
         <Picker
           placeholder="Idade"
-          items={[{
-            value: 'lessThan18',
-            label: 'Menor de 18 anos',
-          }, {
-            value: 'between18and25',
-            label: 'Entre 18 e 25 anos',
-          }, {
-            value: 'between26and35',
-            label: 'Entre 26 e 35 anos',
-          }, {
-            value: 'between36and50',
-            label: 'Entre 36 e 50 anos',
-          }, {
-            value: 'between51and65',
-            label: 'Entre 51 e 65 anos',
-          }, {
-            value: 'over65',
-            label: 'Maior de 65 anos',
-          }]}
+          items={AGE_OPTIONS}
         />
         <Picker
           placeholder="Renda Aproximada"
-          items={[{
-            value: 'belowOne',
-            label: 'Até R$724,00',
-          }, {
-            value: 'fromOneToThree',
-            label: 'De R$724,01 a R$2.172,00',
-          }, {
-            value: 'fromThreeToFive',
-            label: 'De R$2.172,01 a R$3.620,00',
-          }, {
-            value: 'fromFiveToTen',
-            label: 'De R$3.620,01 a R$7.240,00',
-          }, {
-            value: 'fromTenToTwenty',
-            label: 'De R$7.240,01 a R$14.480,00',
-          }, {
-            value: 'overTwenty',
-            label: 'Mais de R$14.480,01',
-          }]}
+          items={WAGE_OPTIONS}
         />
       </View>
     );
