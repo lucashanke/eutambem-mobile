e2367aebcf05954d5e41b5ead93f8a4159117ffd
Lucas Hanke <hankelucas@gmail.com>
2018-03-24 21:15:23 -0300

    [#5]: add index.js to common components module

---
+++ app/components/common/index.js
@@ -0,0 +1,9 @@
+export { default as Button } from './Button';
+export { default as CheckBoxGroup } from './CheckBoxGroup';
+export { default as Container } from './Container';
+export { default as DatePicker } from './DatePicker';
+export { default as Picker } from './Picker';
+export { default as PlacePicker } from './PlacePicker';
+export { default as SectionHeader } from './SectionHeader';
+export { default as SectionText } from './SectionText';
+export { default as TextInput } from './TextInput';
