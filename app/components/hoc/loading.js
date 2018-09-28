322aab07d7744b070be68608eee89451e569e2d8
Lucas Hanke <hankelucas@gmail.com>
2018-09-28 15:34:48 -0300

    add HOC for loading screen

---
+++ app/components/hoc/loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import _ from 'lodash';
+import { SOFT_PURPLE } from '../../styles';
+
+
+export default (WrappedComponent) => (props) => {
+  if (_.isNil(props.data)) {
+    return (
+      <ActivityIndicator size="large" color={SOFT_PURPLE} />
+    );
+  }
+  return (
+    <WrappedComponent {...props} />
+  );
+};
