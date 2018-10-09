a59ca52f1f807cec04db9e6d324a1ea32379a852
Lucas Hanke <hankelucas@gmail.com>
2018-10-09 18:35:25 -0300

    add better testing for picker component

--- app/components/common/__tests__/Picker.test.js
+++ app/components/common/__tests__/Picker.test.js
@@ -2,7 +2,8 @@ import 'react-native';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Picker from '../Picker';
+import { Picker } from '../Picker';
+import Modal from 'react-native-modal';
 
 const props = {
   placeholder: 'Test Picker',
@@ -13,11 +14,34 @@ const props = {
     value: '2',
     label: 'Label 2',
   }],
+  onValueChange: () => {},
 };
 
 describe('Picker', () => {
-  it('renders correctly', () => {
+  it('renders TextInput', () => {
     const wrapper = shallow(<Picker {...props} />);
-    expect(wrapper).toMatchSnapshot();
+
+    expect(wrapper.find('TextInput')).toHaveLength(1);
   });
+
+  it('renders invisible modal at first', () => {
+    const wrapper = shallow(<Picker {...props} />);
+
+    const modal = wrapper.find(Modal);
+
+    expect(modal).toHaveLength(1);
+    expect(modal.props().isVisible).toBeFalsy();
+  });
+
+  describe('openModal', () => {
+    it('renders visible modal when called', () => {
+      const wrapper = shallow(<Picker {...props} />);
+
+      wrapper.instance().openModal();
+      const modal = wrapper.find(Modal);
+
+      expect(modal).toHaveLength(1);
+      expect(modal.props().isVisible).toBeTruthy();
+    });
+  })
 });
