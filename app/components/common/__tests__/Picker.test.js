4f8f6755101b4725d7ee6b67a0995d0a7c4b06be
Lucas Hanke <hankelucas@gmail.com>
2018-10-10 15:57:18 -0300

    add better testing for picker component

--- app/components/common/__tests__/Picker.test.js
+++ app/components/common/__tests__/Picker.test.js
@@ -1,9 +1,11 @@
 import 'react-native';
 import React from 'react';
+import sinon from 'sinon';
 import { shallow } from 'enzyme';
 
 import { Picker } from '../Picker';
 import Modal from 'react-native-modal';
+import { PickerIOS } from 'react-native';
 
 const props = {
   placeholder: 'Test Picker',
@@ -18,10 +20,13 @@ const props = {
 };
 
 describe('Picker', () => {
-  it('renders TextInput', () => {
-    const wrapper = shallow(<Picker {...props} />);
+  it('renders TextInput with label corresponding to the value passed', () => {
+    const wrapper = shallow(<Picker {...props} value="2"/>);
 
-    expect(wrapper.find('TextInput')).toHaveLength(1);
+    const textInput = wrapper.find('TextInput');
+
+    expect(textInput).toHaveLength(1);
+    expect(textInput.props().value).toBe('Label 2');
   });
 
   it('renders invisible modal at first', () => {
@@ -33,15 +38,82 @@ describe('Picker', () => {
     expect(modal.props().isVisible).toBeFalsy();
   });
 
-  describe('openModal', () => {
-    it('renders visible modal when called', () => {
-      const wrapper = shallow(<Picker {...props} />);
+  it('renders invisible modal with a PickerIOS and its options inside of it', () => {
+    const wrapper = shallow(<Picker {...props} />);
 
-      wrapper.instance().openModal();
-      const modal = wrapper.find(Modal);
+    const modal = wrapper.find(Modal);
 
-      expect(modal).toHaveLength(1);
-      expect(modal.props().isVisible).toBeTruthy();
+    expect(modal).toHaveLength(1);
+    const picker = modal.find(PickerIOS);
+    expect(picker).toHaveLength(1);
+    expect(picker.find(PickerIOS.Item)).toHaveLength(2);
+  });
+
+  it('renders visible modal when TextInput is focused', () => {
+    const wrapper = shallow(<Picker {...props} />);
+
+    wrapper.find('TextInput').simulate('focus');
+    const modal = wrapper.find(Modal);
+
+    expect(modal).toHaveLength(1);
+    expect(modal.props().isVisible).toBeTruthy();
+  });
+
+  describe('PickerIOS', () => {
+    describe('when value is changed', () => {
+      it('does not change currentValue state attibute if value is -1', () => {
+        const wrapper = shallow(<Picker {...props} />);
+
+        wrapper.find(PickerIOS).props().onValueChange(-1);
+
+        expect(wrapper.state('currentValue')).toBe('');
+      });
+
+      it('changes currentValue state attibute if value is not -1 ', () => {
+        const wrapper = shallow(<Picker {...props} />);
+
+        wrapper.find(PickerIOS).props().onValueChange(2);
+
+        expect(wrapper.state('currentValue')).toBe(2);
+      });
     });
-  })
+  });
+
+  describe('Modal buttons', () => {
+    describe('Ok button', () => {
+      it('calls onValueChange prop function with default value (first item for IOS) when no current value is defined', () => {
+        const spy = sinon.spy();
+        const wrapper = shallow(<Picker {...props} onValueChange={spy} />);
+
+        wrapper.find({ testID: 'ok-button' }).simulate('press');
+
+        expect(spy.calledWith('1')).toBeTruthy();
+      });
+
+      it('calls onValueChange prop function with current value if is defined', () => {
+        const spy = sinon.spy();
+        const wrapper = shallow(<Picker {...props} onValueChange={spy} />);
+        wrapper.setState({ currentValue: '2' });
+
+        wrapper.find({ testID: 'ok-button' }).simulate('press');
+
+        expect(spy.calledWith('2')).toBeTruthy();
+      });
+    });
+
+    describe('Cancel button', () => {
+      it('does not call onValueChange prop function', () => {
+        const spy = sinon.spy();
+        const wrapper = shallow(<Picker {...props} onValueChange={spy} />);
+        wrapper.setState({ modalVisible: true });
+
+        const modal = wrapper.find(Modal);
+        expect(modal.props().isVisible).toBeTruthy();
+
+        wrapper.find({ testID: 'cancel-button' }).simulate('press');
+
+        expect(spy.callCount).toBe(0);
+      });
+    });
+  });
 });
