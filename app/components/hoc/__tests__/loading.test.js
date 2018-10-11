466338a4d946552e512cb22352983e44c041834b
Lucas Hanke <hankelucas@gmail.com>
2018-10-11 17:42:44 -0300

    add tests for loading HOC

---
+++ app/components/hoc/__tests__/loading.test.js
@@ -0,0 +1,23 @@
+import 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ActivityIndicator } from 'react-native';
+
+import loading from '../loading';
+
+const WrappedComponent = () => (<Text>WrappedComponent</Text>);
+const Loading = loading(WrappedComponent);
+
+describe('loading', () => {
+  it('renders ActivityIndicator when data prop is not defined', () => {
+    const wrapper = shallow(<Loading />);
+    expect(wrapper.find(WrappedComponent)).toHaveLength(0);
+    expect(wrapper.find(ActivityIndicator)).toHaveLength(1);
+  });
+
+  it('renders WrappedComponent when data prop is defined', () => {
+    const wrapper = shallow(<Loading data={'test-data'} />);
+    expect(wrapper.find(WrappedComponent)).toHaveLength(1);
+    expect(wrapper.find(ActivityIndicator)).toHaveLength(0);
+  });
+});
