da483881eaa70e1ad83edd63542a8437681da32d
Lucas Hanke <hankelucas@gmail.com>
2018-10-16 16:12:51 -0300

    [#36] add better testing and remove snapshot testing for SectionHeader

--- app/components/common/__tests__/SectionHeader.test.js
+++ app/components/common/__tests__/SectionHeader.test.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import SectionHeader from '../SectionHeader';
+import { Text } from 'react-native';
 
 const props = {
   title: 'Test SectionHeader',
 };
 
 describe('SectionHeader', () => {
-  it('renders correctly', () => {
+  it('renders Text with title in uppercase', () => {
     const wrapper = shallow(<SectionHeader {...props} />);
-    expect(wrapper).toMatchSnapshot();
+    
+    expect(wrapper.find(Text).children().text()).toEqual(props.title.toUpperCase());
   });
 });
