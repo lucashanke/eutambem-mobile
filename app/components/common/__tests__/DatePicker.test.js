0f867d6dbc457d75824e38789a3d857cb0ad3e22
Lucas Hanke <hankelucas@gmail.com>
2018-10-16 14:14:01 -0300

    [#36] add better testing and remove snapshot testing for DatePicker

--- app/components/common/__tests__/DatePicker.test.js
+++ app/components/common/__tests__/DatePicker.test.js
@@ -1,12 +1,35 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
-import DatePicker from '../DatePicker';
+import { DatePicker } from '../DatePicker';
+import ExternalDatePicker from 'react-native-datepicker';
+
+const props = {
+  onValueChange: () => {},
+  value: '16/Oct/2018',
+}
 
 describe('DatePicker', () => {
-  it('renders correctly', () => {
-    const wrapper = shallow(<DatePicker />);
-    expect(wrapper).toMatchSnapshot();
+  it('renders external datepicker component', () => {
+    const wrapper = shallow(<DatePicker {...props} />);
+    
+    expect(wrapper.find(ExternalDatePicker)).toHaveLength(1);
+  });
+
+  it('renders passes value prop to external component date prop', () => {
+    const wrapper = shallow(<DatePicker {...props} />);
+    
+    expect(wrapper.find(ExternalDatePicker).props().date).toEqual('16/Oct/2018');
+  });
+
+  it('calls onValueChange prop when date changes on external component', () => {
+    const onValueChange = sinon.spy();
+    const wrapper = shallow(<DatePicker {...props} onValueChange={onValueChange} />);
+    
+    wrapper.find(ExternalDatePicker).props().onDateChange('17/Oct/2018');
+    
+    expect(onValueChange.calledWith('17/Oct/2018')).toBeTruthy();
   });
 });
