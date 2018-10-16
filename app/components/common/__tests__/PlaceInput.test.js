193c35c7f7ea7cdc6a3fd04e3e8e10a8aba11c6f
Lucas Hanke <hankelucas@gmail.com>
2018-10-16 14:50:12 -0300

    [#36] add better testing and remove snapshot testing for PlaceInput

--- app/components/common/__tests__/PlaceInput.test.js
+++ app/components/common/__tests__/PlaceInput.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import PlaceInput from '../PlaceInput';
+import { PlaceInput } from '../PlaceInput';
 
 const props = {
   placeScreenTitle: 'Inform the place',
@@ -17,8 +17,29 @@ const props = {
 };
 
 describe('PlaceInput', () => {
-  it('renders correctly', () => {
+  it('renders a TextInput', () => {
     const wrapper = shallow(<PlaceInput {...props} />);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('TextInput')).toHaveLength(1);
+  });
+
+  it('focus of the TextInput calls the navigate method to the PlacePicker screen', () => {
+    const wrapper = shallow(<PlaceInput {...props} />);
+
+    wrapper.find('TextInput').simulate('focus');
+   
+    expect(props.navigation.navigate.calledWith('PlaceScreen')).toBeTruthy();
+  });
+
+  it('navigation passes onPlaceChange function that calls onValueChange prop', () => {
+    const wrapper = shallow(<PlaceInput {...props} />);
+
+    const { onPlaceChange } = wrapper.props().navigation.navigate.args[0][1];
+    onPlaceChange({ id: 'testPlace', label: 'place for testing' })
+   
+    expect(props.onValueChange.callCount).toEqual(1);
+    expect(props.onValueChange.calledWith({
+      id: 'testPlace', label: 'place for testing',
+    })).toBeTruthy();
+    expect(props.navigation.goBack.callCount).toEqual(1);
   });
 });
