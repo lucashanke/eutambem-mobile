89d598cde9aedb9b1ce0406923434d951014ebfd
Lucas Hanke <hankelucas@gmail.com>
2018-10-16 16:28:20 -0300

    [#36] add better testing and remove snapshot testing for TextInput

--- app/components/common/__tests__/TextInput.test.js
+++ app/components/common/__tests__/TextInput.test.js
@@ -1,16 +1,39 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
-import TextInput from '../TextInput';
+import { TextInput } from '../TextInput';
+import { TextInput as NativeTextInput } from 'react-native';
 
 const props = {
   placeholder: 'Test TextInput',
+  onValueChange: sinon.spy(),
 };
 
 describe('TextInput', () => {
-  it('renders correctly', () => {
+  it('renders native TextInput', () => {
     const wrapper = shallow(<TextInput {...props} />);
-    expect(wrapper).toMatchSnapshot();
+    
+    expect(wrapper.find(NativeTextInput)).toHaveLength(1);
+  });
+
+  it('onChangeText of native TextInput calls onValueChange prop', () => {
+    const wrapper = shallow(<TextInput {...props} />);
+
+    wrapper.find(NativeTextInput).props().onChangeText('new text');
+    
+    expect(props.onValueChange.calledWith('new text')).toBeTruthy();
+  });
+
+  it('sets multiline and autoGrow to true in native input if multiline prop is true', () => {
+    const wrapper = shallow(<TextInput {...props} multiline />);
+
+    const nativeInput = wrapper.find(NativeTextInput);
+
+    nativeInput.props().onChangeText('new text');
+    
+    expect(nativeInput.props().multiline).toBeTruthy();
+    expect(nativeInput.props().autoGrow).toBeTruthy();
   });
 });
