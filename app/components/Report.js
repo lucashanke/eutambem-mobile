6d95bd582857c9a230331f1b23a6ad4bd5c0fbaa
Lucas Hanke <hankelucas@gmail.com>
2018-10-26 17:28:00 -0300

    #23 fix labels of values in Report component

--- app/components/Report.js
+++ app/components/Report.js
@@ -3,27 +3,62 @@ import { View } from 'react-native';
 
 import { InputReview, SectionHeader } from './common';
 
-const Report = (props) => {
-  const { data } = props;
+const getLabel = (key, value, options) => {
+  const option = options[key].filter((option) => option.value === value)[0];
+  return option ? option.label : '';
+}
+
+export const Report = (props) => {
+  const { data, options } = props;
   return (
     <View>
       <SectionHeader title="Sobre o ocorrido" />
-      <InputReview label="Empresa em que ocorreu" value={data.establishment.label} />
-      <InputReview label="Tipo de assédio" value={data.harassmentType} />
-      <InputReview label="Descrição" value={data.description} />
-      <InputReview label="Quando ocorreu" value={data.date} />
-      <InputReview label="Providências tomadas" value={data.followupActions} />
-      <InputReview label="Recomendaria essa empresa" value={data.wouldRecommend} />
-      <InputReview label="Conselho para os gestores" value={data.advice} />
+      <InputReview
+        label="Empresa em que ocorreu"
+        value={data.establishment.label} />
+      <InputReview
+        label="Tipo de assédio"
+        value={getLabel('harassment_type_options',data.harassmentType, options)} />
+      <InputReview
+        label="Descrição"
+        value={data.description} />
+      <InputReview
+        label="Quando ocorreu"
+        value={data.date} />
+      <InputReview
+        label="Providências tomadas"
+        value={data.followupActions.map(
+          (action) => getLabel('followup_actions_options', action, options)
+        )} />
+      <InputReview
+        label="Recomendaria essa empresa"
+        value={getLabel('yes_no_optional_options', data.wouldRecommend, options)} />
+      <InputReview
+        label="Conselho para os gestores"
+        value={data.advice} />
 
       <SectionHeader title="Sobre você" />
-      <InputReview label="Gênero" value={data.gender} />
-      <InputReview label="Cor" value={data.skinColor} />
-      <InputReview label="Idade" value={data.age} />
-      <InputReview label="Orientação sexual" value={data.sexualOrientation} />
-      <InputReview label="Renda" value={data.wage} />
-      <InputReview label="Email" value={data.email} />
-      <InputReview label="Nome" value={data.name} />
+      <InputReview
+        label="Gênero"
+        value={getLabel('gender_options', data.gender, options)} />
+      <InputReview
+        label="Cor"
+        value={getLabel('skin_color_options', data.skinColor, options)} />
+      <InputReview
+        label="Idade"
+        value={getLabel('age_options', data.age, options)} />
+      <InputReview
+        label="Orientação sexual"
+        value={getLabel('sexual_orientation', data.sexualOrientation, options)} />
+      <InputReview
+        label="Renda"
+        value={getLabel('wage_options', data.wage, options)} />
+      <InputReview
+        label="Email"
+        value={data.email} />
+      <InputReview
+        label="Nome"
+        value={data.name} />
     </View>
   );
 };
