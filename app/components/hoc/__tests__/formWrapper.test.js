90d534093bff8f2a44965a467e443747529db702
Lucas Hanke <hankelucas@gmail.com>
2018-10-26 16:31:25 -0300

    #23 change formWrapper to save options in local storage (AsyncStorage)

--- app/components/hoc/__tests__/formWrapper.test.js
+++ app/components/hoc/__tests__/formWrapper.test.js
@@ -5,19 +5,49 @@ import sinon from 'sinon';
 
 import formWrapper from '../formWrapper';
 import * as reportService from '../../../services/reportService';
+import * as storageService from '../../../services/storageService';
+
 
 const WrappedComponent = () => (<Text>WrappedComponent</Text>);
 const FormWrapper = formWrapper(WrappedComponent);
 
+const retrieveOptionsStub = sinon.stub(storageService, 'retrieveOptions');
 
 describe('formWrapper', () => {
+
+  beforeEach(() => {
+    retrieveOptionsStub.reset();
+  });
 
   it('always renders WrappedComponent', () => {
     const wrapper = shallow(<FormWrapper />);
     expect(wrapper.find(WrappedComponent)).toHaveLength(1);
   });
 
-  it('passes down formOption in data prop', async () => {
+  it('tries to get form options form local storage', async () => {
+    await shallow(<FormWrapper />);
+
+    expect(retrieveOptionsStub.callCount).toEqual(1);
+    retrieveOptionsStub.reset();
+  });
+
+  it('if local storage has options, it passes them down in data prop', async () => {
+    retrieveOptionsStub.resolves({
+      options: [{ value: 1 , label: 'Value 1 - from local' }, { value: 2 , label: 'Value 2 - from local' }]
+    });
+
+    const wrapper = await shallow(<FormWrapper />);
+
+    expect(wrapper.find(WrappedComponent).props().data).toEqual({ formOptions: {
+      options: [{ value: 1 , label: 'Value 1 - from local' }, { value: 2 , label: 'Value 2 - from local' }]
+    }});
+
+    retrieveOptionsStub.reset();
+  });
+
+  it('if local storage has no options, it fetches from reportService and passes them down in data prop', async () => {
+    retrieveOptionsStub.resolves(null);
+    
     const stub = sinon.stub(reportService, 'fetchReportFormConstants');
     stub.resolves({
       constants: {
@@ -26,9 +56,21 @@ describe('formWrapper', () => {
     });
 
     const wrapper = await shallow(<FormWrapper />);
+    await wrapper.update();
 
     expect(wrapper.find(WrappedComponent).props().data).toEqual({ formOptions: {
       options: [{ value: 1 , label: 'Value 1' }, { value: 2 , label: 'Value 2' }],
     }});
+  });
+
+  it('saves options to local storage', async () => {
+    const spy = sinon.spy(storageService, 'storeOptions');
+
+    retrieveOptionsStub.resolves(null);
+
+    const wrapper = await shallow(<FormWrapper />);
+    await wrapper.update();
+
+    expect(spy.callCount).toEqual(1);
   });
 });
---
+++ app/services/storageService.js
@@ -0,0 +1,17 @@
+import { AsyncStorage } from "react-native"
+
+export const retrieveOptions = async () => {
+  try {
+    return JSON.parse(await AsyncStorage.getItem('@EuTambemStore:options'));
+  } catch (error) {
+    return null;
+  }
+};
+
+export const storeOptions = async (options) => {
+  try {
+    await AsyncStorage.setItem('@EuTambemStore:options', JSON.stringify(options));
+  } catch (error) {
+    console.log(error);
+  }
+};
